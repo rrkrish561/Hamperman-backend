f35ffac0d7a49f3c9b4ecf7df8d430ada2465cc8
rrkrish561 <rrkrish561@gmail.com>
2020-12-09 17:39:09 -0500

    heroku

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+export default {
+    db: {
+      uri:
+        "mongodb+srv://rradhak1:Krishna561@cluster0.ulgen.mongodb.net/HampermanHighScores?retryWrites=true&w=majority",
+    },
+    port: 5000,
+  };
